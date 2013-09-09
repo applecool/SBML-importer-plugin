572a34b27000d72621b5efea15a9bf34a50b024a
applecool <applecoolkmit@gmail.com>
2013-09-09 22:52:31 +0530

    updated fructRein

--- src/org/pathvisio/sbml/FruchtRein.java
+++ src/org/pathvisio/sbml/FruchtRein.java
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and 
 // limitations under the License.
 //
-package org.pathvisio.sbml;
+/*package org.pathvisio.sbml;
 
 import java.awt.Dimension;
 import java.awt.geom.Point2D;
@@ -56,3 +56,4 @@ public class FruchtRein extends LayoutAbstract{
 		drawLines();
 	}
 }
+*/
